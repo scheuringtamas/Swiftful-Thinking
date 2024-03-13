6c6dd5dca3dfb0f202bd789587fe17ee7e56bf88
Tamás Scheuring <102406235+scheuringtamas@users.noreply.github.com>
2024-03-13 09:52:32 +0100

    Add files via upload

---
+++ SwiftfulMapApp/SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,23 @@
+import Foundation
+import MapKit
+
+struct Location: Identifiable, Equatable {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    // Identifiable
+    var id: String {
+        name + cityName
+    }
+
+    // Equatable
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+
+}
---
+++ SwiftfulMapApp/SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    @StateObject private var vm = LocationsViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            LocationsView()
+                .environmentObject(vm)
+        }
+    }
+}
